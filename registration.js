df2ce71ef2e65da23bb2ae44614aa02174347cde
vdub <vungh.waan@gmail.com>
2022-11-14 17:18:03 -0600

    consolidated css, created box for diary's table, added hamburger menu, updated registration and login

---
+++ registration.js
@@ -0,0 +1,94 @@
+const submitBtn = document.getElementById('submit-btn');
+const loginBtn = document.getElementById('login-btn');
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+submitBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+// write code for login-btn:
+// when the login button is clicked
+// validate the email and password exist in database
+// if it exist; go to my-daily.html
+// if email and password doesn't exit, go to Create Account
+
+
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const firstNameValue = firstName.value.trim();
+    const lastNameValue = lastName.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if(firstNameValue === '') {
+        setError(firstName, 'First name is required');
+    } else {
+        setSuccess(firstName);
+    }
+
+    if(lastNameValue === '') {
+        setError(lastName, 'Last name is required');
+    } else {
+        setSuccess(lastName);
+    }
+
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+
+    if(passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 8 ) {
+        setError(password, 'Password must be at least 8 characters.')
+    } else {
+        setSuccess(password);
+    }
+
+    if(password2Value === '') {
+        setError(password2, 'Please confirm your password');
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "Passwords doesn't match");
+    } else {
+        setSuccess(password2);
+    }
+
+};
+
+
+
+
+
